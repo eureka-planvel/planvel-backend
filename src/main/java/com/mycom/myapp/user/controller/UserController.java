3b4f7239ad917881839acb310a1c56e5d34a7e1b
hyunnk <jhyunnk30@gmail.com>
2025-05-09 12:04:50 +0900

    feat: register 부분 Controller, user 패키지 진행중

--- src/main/java/com/mycom/myapp/user/controller/UserController.java
+++ src/main/java/com/mycom/myapp/user/controller/UserController.java
@@ -3,7 +3,7 @@ package com.mycom.myapp.user.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mycom.myapp.user.dto.UserResultDto;
+//import com.mycom.myapp.user.dto.UserResultDto;
 import com.mycom.myapp.user.entity.User;
 import com.mycom.myapp.user.service.UserService;
 
@@ -15,9 +15,9 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 	private final UserService userService;
 
-	@PostMapping("/register")
-	public UserResultDto insertUser(User user) { // User Entity 사용
-		return userService.insertUser(user);
-	}
+//	@PostMapping("/register")
+//	public UserResultDto insertUser(User user) { // User Entity 사용
+//		return userService.insertUser(user);
+//	}
 	
 }
