186281e971bb1c6c2ee1250971cf82bb851c827b
hyunnk <jhyunnk30@gmail.com>
2025-05-09 11:43:23 +0900

    feat: register controller, user 패키지 작성중

---
+++ src/main/java/com/mycom/myapp/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.mycom.myapp.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycom.myapp.user.dto.UserResultDto;
+import com.mycom.myapp.user.entity.User;
+import com.mycom.myapp.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+	private final UserService userService;
+
+	@PostMapping("/register")
+	public UserResultDto insertUser(User user) { // User Entity 사용
+		return userService.insertUser(user);
+	}
+	
+}
---
+++ src/main/java/com/mycom/myapp/user/entity/User.java
@@ -0,0 +1,28 @@
+package com.mycom.myapp.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="users")
+@Setter
+@Getter
+@ToString
+public class User {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String name;
+	private String email;
+	private String password;
+	
+	@Column(name="profile_img")
+	private String profileImg;
+}
