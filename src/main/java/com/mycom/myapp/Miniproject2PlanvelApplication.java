f236a8141b63042d339a1ffa0cda56c3df406306
Jimin Byun <fraise3838@naver.com>
2025-05-09 10:00:34 +0900

    chore : initi spring project

---
+++ src/main/java/com/mycom/myapp/Miniproject2PlanvelApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniproject2PlanvelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Miniproject2PlanvelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycom/myapp/Miniproject2PlanvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniproject2PlanvelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
