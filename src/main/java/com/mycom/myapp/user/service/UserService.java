e6c7df4ff69fab6b6b497fcb86e1a403aa4a53e3
hyunnk <jhyunnk30@gmail.com>
2025-05-13 09:47:04 +0900

    chore: controller와 service 인증 로직 분리

--- src/main/java/com/mycom/myapp/user/service/UserService.java
+++ src/main/java/com/mycom/myapp/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.mycom.myapp.user.service;
 
+import com.mycom.myapp.auth.dto.response.LoginResponseDto;
 import com.mycom.myapp.user.dto.ChangePasswordRequestDto;
 import com.mycom.myapp.user.dto.UserProfileResponseDto;
 import com.mycom.myapp.user.dto.UserRegisterRequestDto;
@@ -10,9 +11,9 @@ public interface UserService {
 
 	boolean isEmailDuplicate(String email);
 
-	UserProfileResponseDto getUserProfile();
+	UserProfileResponseDto getUserProfile(LoginResponseDto loginUser);
 
 	void changePassword(int userId, ChangePasswordRequestDto requestDto);
 
-	UserProfileResponseDto updateUserProfile(String name, MultipartFile profileImage);
+	UserProfileResponseDto updateUserProfile(LoginResponseDto loginUser, String name, MultipartFile profileImage);
 }
--- src/main/java/com/mycom/myapp/user/service/UserServiceImpl.java
+++ src/main/java/com/mycom/myapp/user/service/UserServiceImpl.java
@@ -66,14 +66,7 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public UserProfileResponseDto getUserProfile() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal() instanceof String) {
-			throw new IllegalStateException("인증된 사용자가 없습니다.");
-		}
-
-		LoginResponseDto loginUser = (LoginResponseDto) authentication.getPrincipal();
-
+	public UserProfileResponseDto getUserProfile(LoginResponseDto loginUser) {
 		User user = userRepository.findById(loginUser.getId())
 				.orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
@@ -86,14 +79,7 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public UserProfileResponseDto updateUserProfile(String name, MultipartFile imageFile) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal() instanceof String) {
-			throw new IllegalStateException("인증된 사용자가 없습니다.");
-		}
-
-		LoginResponseDto loginUser = (LoginResponseDto) authentication.getPrincipal();
-
+	public UserProfileResponseDto updateUserProfile(LoginResponseDto loginUser, String name, MultipartFile imageFile) {
 		User user = userRepository.findById(loginUser.getId())
 				.orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
