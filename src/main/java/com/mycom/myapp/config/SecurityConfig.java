65ae3ab23029e1c56f65bdb75ecf1254a4e3c482
Jimin Byun <fraise3838@naver.com>
2025-05-14 12:00:51 +0900

    fix: permitAll() "/api/review/**" 제거

--- src/main/java/com/mycom/myapp/config/SecurityConfig.java
+++ src/main/java/com/mycom/myapp/config/SecurityConfig.java
@@ -24,8 +24,7 @@ public class SecurityConfig {
 								"/api/user/email-check",
 								"/api/auth/login",
 								"/api/auth/logout",
-								"/api/review",
-								"/api/review/**"
+								"/api/review"
 						).permitAll()
 						.requestMatchers("/api/user/*/password").permitAll()
 						.anyRequest().authenticated()
